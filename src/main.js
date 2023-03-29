227745a343d2034a01730bc2c3e64e9171403c64
skullblack <apichot_5068@inventage.co>
2023-03-29 13:37:06 +0700

    up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>;
 // ================ example use init script =================
 // (function () {
 //   const arguments = {
@@ -48,10 +49,30 @@ function genDealId() {
   return deal_id;
 }
 
+function appendUserAgent(PXID) {
+  const l = window.location;
+  const px = PXID + '|' + window.navigator.userAgent + '|' + l.protocol + '//' + l.host + l.pathname;
+  return px;
+}
+
 function clearDataLocalStorage(fields) {
   fields.map(field => {
     localStorage.setItem(field, '');
   });
+}
+
+function validatePhone(phone) {
+  if (!phone) return undefined;
+  phone = phone.replace(/(\s+|-|\+66|^66|^0)/g, '0');
+  if (phone.length !== 10) return undefined;
+  return phone;
+}
+function correctName(name) {
+  name = name
+    .replace(/^(นาย|นางสาว|น.ส.|ด.ช.|นาง|คุณ |เด็กชาย|เด็กหญิง)/g, '')
+    .replace(/\s\s+/g, ' ')
+    .trim();
+  return name;
 }
 
 function listenerForm() {
@@ -90,22 +111,136 @@ function listenerForm() {
   );
 }
 
-function validatePhone(phone) {
-  if (!phone) return undefined;
-  phone = phone.replace(/(\s+|-|\+66|^66|^0)/g, '0');
-  if (phone.length !== 10) return undefined;
-  return phone;
-}
-function correctName(name) {
-  name = name
-    .replace(/^(นาย|นางสาว|น.ส.|ด.ช.|นาง|คุณ |เด็กชาย|เด็กหญิง)/g, '')
-    .replace(/\s\s+/g, ' ')
-    .trim();
-  return name;
+//=======================================================================
+
+async function submitPayment() {
+  const {ip} = await getIp();
+
+  var {
+    email,
+    tel,
+    fullName,
+    course,
+    seller,
+    campaign,
+    query,
+    dealId,
+    px,
+    redirect_url,
+    discountCode,
+    callback_url,
+    price,
+  } = getDataFromLocalStorage();
+
+  const redirectQuery = new URLSearchParams({
+    dealId,
+    email,
+    fullName,
+    phone: tel,
+    price,
+    discountCode,
+  }).toString();
+
+  var courses = course ? course.split(',') : [];
+  if (courses.length) {
+    var cartItems = courses.map(c => {
+      return {
+        product: c,
+        quantity: 1,
+      };
+    });
+    var data = {
+      cartItems,
+      userdata: {
+        email: email,
+        tel: tel || '',
+        fullName: fullName || '',
+      },
+      cartTracking: {
+        convertionId: conversion?.hash || '',
+        campaign: campaign || '',
+        seller: seller || '',
+        channel: 'SGC',
+        ip,
+        utm_source: query.utm_source || '',
+        utm_medium: query.utm_medium || '',
+        utm_campaign: query.utm_campaign || '',
+        utm_term: query.utm_term || '',
+        utm_content: query.utm_content || '',
+        customField1: dealId,
+        customField2: px,
+      },
+      paymentSuccessRedirectUrl: `${redirect_url}?${redirectQuery}`,
+    };
+
+    if (callback_url) data.paymentSuccessCallbackUrl = callback_url;
+
+    var url = await createCart(data);
+
+    if (discountCode) url = `${url}?discountCode=${discountCode}`;
+    setTimeout(function () {
+      window.location.replace(url);
+    }, 1500);
+  }
 }
 
-function appendUserAgent(PXID) {
-  const l = window.location;
-  const px = PXID + '|' + window.navigator.userAgent + '|' + l.protocol + '//' + l.host + l.pathname;
-  return px;
+function getDataFromLocalStorage() {
+  var email = localStorage.getItem('email');
+  var tel = localStorage.getItem('phone');
+  var fullName = localStorage.getItem('name');
+  var price = localStorage.getItem('price');
+  var course = localStorage.getItem('course');
+  var seller = localStorage.getItem('seller');
+  var campaign = localStorage.getItem('campaign');
+  var dealId = localStorage.getItem('deal_id');
+  var px = localStorage.getItem('px');
+  var redirect_url = localStorage.getItem('redirect_url');
+  var callback_url = localStorage.getItem('callback_url');
+  var discountCode = localStorage.getItem('discountCode');
+  var query = JSON.parse(localStorage.getItem('params') || {});
+  return {
+    email,
+    tel,
+    fullName,
+    course,
+    seller,
+    campaign,
+    dealId,
+    query,
+    px,
+    redirect_url,
+    price,
+    discountCode,
+    callback_url,
+  };
+}
+
+async function createCart(cart) {
+  var data = await fetchPost('https://pay-api.futureskill.co/api/cart/create', cart, {
+    'Content-Type': 'application/json',
+    Authorization: 'Basic NzAyMTg2NzcwNzY0ODMzMDQ6ejE5ZmpmckZrMGZuUVFRTUc0UHQ=',
+  });
+  return data.url;
+}
+
+async function getIp() {
+  const response = await fetch('https://www.cloudflare.com/cdn-cgi/trace');
+  let data = await response.text();
+  data = data
+    .trim()
+    .split('\n')
+    .reduce(function (obj, pair) {
+      pair = pair.split('=');
+      return (obj[pair[0]] = pair[1]), obj;
+    }, {});
+  return data;
+}
+
+async function fetchPost(url, data, headers) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
