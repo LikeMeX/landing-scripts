0e1821158f749153c32ffa23c2e3af52e61ddb88
Onchulee <onchulee.wip@hotmail.com>
2025-11-24 11:42:22 +0700

    refactor: 💡 change api key and add script handle error status
    1. change api key because old key unable to use 2. add body to handle
    error status for  future change

--- src/main.js
+++ src/main.js
@@ -343,29 +343,40 @@ function validatePhone(phone, feildName) {
 
 async function validateEmailWithZeroBounce(email) {
   if (!email) return false;
-  const api_key = "85b5a9d5f22746b4906a30a7a33fe7ff";
-  const response = await fetch(
-    `https://api.zerobounce.net/v2/validate?api_key=${api_key}&email=${email}`
-  );
-  const responseData = await response.json();
-  if (responseData && responseData.status) {
-    switch (responseData.status) {
-      case "valid":
-        return true;
-      case "catch":
-        return true;
-      case "do_not_mail":
-        if (
-          responseData.sub_status === "role_based" ||
-          responseData.sub_status === "role_based_catch_all"
-        )
-          return true;
-        else return false;
+  const api_key = "a1e92001ee1549908b7a48cd3b69c180";
+  try {
+      const response = await fetch(
+      `https://api.zerobounce.net/v2/validate?api_key=${api_key}&email=${email}`
+    );
+    if (response.ok) { 
+      const responseData = await response.json();
+        if (responseData && responseData.status) {
+          switch (responseData.status) {
+            case "valid":
+              return true;
+            case "catch":
+              return true;
+            case "do_not_mail":
+              if (
+                responseData.sub_status === "role_based" ||
+                responseData.sub_status === "role_based_catch_all"
+              )
+                return true;
+              else return false;
 
-      default:
-        return false;
+            default:
+              return false;
+          }
+        }
     }
+    else if () { 
+
+    }
+  } catch (error) {
+    // Handle network errors or other issues that prevent a response
+    console.error('Fetch error:', error);
   }
+  
   return false;
 }
 
